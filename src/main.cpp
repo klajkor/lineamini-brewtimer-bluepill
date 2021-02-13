f576d100b2a17e89825b946d76d62d0ec5cf918c
Robert Klajko <klajkor@gmail.com>
2021-02-13 20:57:55 +0100

    Display update

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 * @author Robert Klajko
 * @url https://github.com/klajkor/lineamini-brewtimer-bluepill.git
 *
-* Board: Arduino Pro Mini/Nano
+* Board: STM32 BluePill (generic STM32F103C8) with Arduino Framework
 *
 * Extension modules and hw used:
 *  - SSD1306 OLED display, I2C - optional, please see source code for feature switch (SSD1306_ENABLED)
@@ -67,6 +67,8 @@
   #include <MCUFRIEND_kbv.h> // for ILI9340
   #include <mcufriend_special.h>
   #include <Fonts/FreeSans12pt7b.h>
+  #include <Fonts/FreeSans18pt7b.h>
+  #include <Fonts/FreeSans24pt7b.h>
   // Assign human-readable names to some common 16-bit color values:
   #define	ILI9340_BLACK   0x0000
   #define	ILI9340_BLUE    0x001F
@@ -78,16 +80,21 @@
   #define ILI9340_WHITE   0xFFFF
   #define ILI9340_DISPLAY_CLEAR_COLOR ILI9340_BLACK
   //Screen coordinates
-  #define ILI9340_TIMER_POS_X 50
+  #define ILI9340_TIMER_POS_X 45
   #define ILI9340_TIMER_POS_Y 80
   #define ILI9340_TIMER_WIDTH 240
   #define ILI9340_TIMER_HEIGTH 80
   #define ILI9340_TIMER_Y_OFFSET 5
-  #define ILI9340_TEMPERATURE_POS_X 20
-  #define ILI9340_TEMPERATURE_POS_Y 180
-  #define ILI9340_TEMPERATURE_WIDTH 200
+  #define ILI9340_TEMPERATURE_POS_X 25
+  #define ILI9340_TEMPERATURE_POS_Y 170
+  #define ILI9340_TEMPERATURE_WIDTH 310
   #define ILI9340_TEMPERATURE_HEIGTH 60
   #define ILI9340_TEMPERATURE_Y_OFFSET 3
+  #define ILI9340_MILLI_VOLT_POS_X 55
+  #define ILI9340_MILLI_VOLT_POS_Y 215
+  #define ILI9340_MILLI_VOLT_WIDTH 250
+  #define ILI9340_MILLI_VOLT_HEIGTH 20
+  #define ILI9340_MILLI_VOLT_Y_OFFSET 2
 #endif  // #ifdef ILI9340_ENABLED
 
 #ifdef SSD1306_ENABLED
@@ -152,15 +159,13 @@
 #define TEMPERATURENOMINAL 25
 // how many samples to take and average, more takes longer
 // but is more 'smooth'
-#define NUMSAMPLES 100
+#define NUMSAMPLES 5
 // The beta coefficient of the thermistor (usually 3000-4000)
 #define BCOEFFICIENT_V1 4400  // version 1
 #define BCOEFFICIENT_V2 3977  // version 2, updated calculation
 // the value of the 'other' resistor
 #define SERIESRESISTOR_V1 6960
 #define SERIESRESISTOR_V2 6190  // version 2, measured on board
-//scaling value to convert voltage
-#define VOLTAGESCALE 12.1
 //reference voltage
 //#define VOLTAGEREF 4.585
 #define VOLTAGEREF 4.16
@@ -194,7 +199,7 @@ int virtual_Reed_Switch = VIRT_REED_SWITCH_OFF; // virtual switch
 int state_Volt_Meter = 0;
 unsigned long t_Volt_Meter = 0;
 unsigned long t_0_Volt_Meter = 0;
-unsigned long delay_Between_2_Measures = 300;
+unsigned long delay_Between_2_Measures = 500;
 
 //SM Display variables
 int state_Display = 0;
@@ -220,8 +225,8 @@ int pin_Status_Led = LED_BUILTIN;
 int val_Status_Led = 0;
 unsigned long t_Status_Led = 0;
 unsigned long t_0_Status_Led = 0;
-unsigned long on_delay_Status_Led = 100;
-unsigned long off_delay_Status_Led = 100;
+unsigned long on_delay_Status_Led = 50;
+unsigned long off_delay_Status_Led = 50;
 int beep_count_Status_Led = 0;
 int beep_number_Status_Led = 2;
 
@@ -229,8 +234,6 @@ char TimeCounterStr[] = "00:00"; /** String to store time counter value, format:
 
 // Current and voltage sensor class
 Adafruit_INA219 ina219_monitor;
-#define INA219_VCC_PIN 3
-#define INA219_GND_PIN 2
 
 // INA219 sensor variables
 float bus_Voltage_V;    /** Measured bus voltage in V*/
@@ -243,7 +246,6 @@ char temperature_String_V1[] = "  999.9";
 float steinhart = 0.0;
 float calc_Temperature_V2 = 0.0;
 char temperature_String_V2[] = "999.9";
-char temperature_String_V2_Led_Matrix[] = "999.9";
 float thermistor_Res = 0.00; // Thermistor calculated resistance
 
 /* Function declarations */
@@ -267,6 +269,7 @@ void display_Timer_On_Ssd1306(bool need_Display_Clear,bool need_Display_Stopped)
 void display_Timer_On_ILI9340(bool need_Display_Clear,bool need_Display_Stopped);
 void display_Temperature_On_Ssd1306(void);
 void display_Temperature_On_ILI9340(void);
+void display_Milli_Volt_On_ILI9340(void);
 
 void ina219_Init(void);
 void get_Voltage(void);
@@ -297,12 +300,12 @@ void setup() {
   Ssd1306_Oled_Init();
   #endif
   // SM inits
+  Display_Clear();
   StateMachine_counter1();
   StateMachine_Reed_Switch();
   StateMachine_Volt_Meter();
   state_Display = DISPLAY_STATE_RESET;
   StateMachine_Display();
-  Display_Clear();
   //Adafruit_Text_Display_Test();
 
 }
@@ -580,16 +583,23 @@ void get_Voltage(void)
 void calculate_Temperature_V2(void) {
   thermistor_Res = SERIESRESISTOR_V2 * (1/((LMREF / bus_Voltage_V) -1));
   steinhart = thermistor_Res / THERMISTORNOMINAL_V2;
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT_V2;                   // 1/B * ln(R/Ro)
-  steinhart += (1.0 / (TEMPERATURENOMINAL + 273.15)); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  calc_Temperature_V2 = (float) steinhart - 273.15;                         // convert to C
-  dtostrf(calc_Temperature_V2, 5, 1, temperature_String_V2);
-  dtostrf(calc_Temperature_V2, 5, 1, temperature_String_V2_Led_Matrix);
+  steinhart = log(steinhart);                          // ln(R/Ro)
+  steinhart /= BCOEFFICIENT_V2;                        // 1/B * ln(R/Ro)
+  steinhart += (1.0 / (TEMPERATURENOMINAL + 273.15));  // + (1/To)
+  steinhart = (float) (1.0 / steinhart);               // Invert
+  calc_Temperature_V2 = (float) (steinhart - 273.15);  // convert to C
+  if (bus_Voltage_V >= 1.0 and bus_Voltage_V <= 4.0) 
+  {
+    dtostrf(calc_Temperature_V2, 5, 1, temperature_String_V2);  
+  }
+  else 
+  {
+    strcpy(temperature_String_V2, "Error");
+  }
+  
   // debug display
   #ifdef SERIAL_DEBUG_ENABLED
-  Serial.print(temperature_String_V1);
+  Serial.print(temperature_String_V2);
   Serial.println(F(" *C"));
   #endif
 }
@@ -608,7 +618,7 @@ void StateMachine_Volt_Meter(void) {
 
     case VOLT_METER_STATE_STOP_TIMER:
       t_Volt_Meter = millis();
-      if (t_Volt_Meter - t_0_Volt_Meter > delay_Between_2_Measures) {
+      if (t_Volt_Meter - t_0_Volt_Meter >= delay_Between_2_Measures) {
         state_Volt_Meter = VOLT_METER_STATE_READ_VOLTAGE;
       }
       break;
@@ -621,9 +631,7 @@ void StateMachine_Volt_Meter(void) {
     case VOLT_METER_STATE_CONVERT_TO_TEMPERATURE:
       calculate_Temperature_V2();
       state_Volt_Meter = VOLT_METER_STATE_START_TIMER;
-      if (state_counter1 == COUNTER_STATE_DISABLED && state_Display != DISPLAY_STATE_HOLD_TIMER_ON) {
-        state_Display = DISPLAY_STATE_TEMPERATURE;
-      }
+      state_Display = DISPLAY_STATE_TEMPERATURE;      
       break;
   }
 }
@@ -633,19 +641,19 @@ void StateMachine_Display(void) {
   switch (state_Display) {
     case DISPLAY_STATE_RESET:
       Display_Stopped_Timer();
-      state_Display = DISPLAY_STATE_DO_NOTHING;
+      state_Display = DISPLAY_STATE_TEMPERATURE;
       break;
 
     case DISPLAY_STATE_TIMER_RUNNING:
       Display_Running_Timer();
-      Display_Temperature();
-      state_Display = DISPLAY_STATE_DO_NOTHING;
+      //Display_Temperature();
+      state_Display = DISPLAY_STATE_TEMPERATURE;
       break;
 
     case DISPLAY_STATE_TIMER_STOPPED:
       Display_Stopped_Timer();
       t_0_Display = millis();
-      state_Display = DISPLAY_STATE_HOLD_TIMER_ON;      
+      state_Display = DISPLAY_STATE_TEMPERATURE;      
       break;
 
     case DISPLAY_STATE_HOLD_TIMER_ON:
@@ -679,6 +687,7 @@ void Display_Temperature(void) {
   #endif
   #ifdef ILI9340_ENABLED
   display_Temperature_On_ILI9340();
+  display_Milli_Volt_On_ILI9340();
   #endif
 }
 
@@ -752,8 +761,8 @@ void ILI9340_Init(void) {
   tft_ili9340.setCursor(0, 40);
   //uint16_t wid = tft_ili9340.width();
   tft_ili9340.setTextColor(ILI9340_WHITE);  
-  tft_ili9340.setFont(&FreeSans12pt7b);
-  tft_ili9340.setTextSize(2);
+  tft_ili9340.setFont(&FreeSans24pt7b);
+  tft_ili9340.setTextSize(1);
   tft_ili9340.println("Linea Mini ");
   tft_ili9340.println("Brew Timer ");
   tft_ili9340.println("Version 1.0");
@@ -769,8 +778,8 @@ void display_Timer_On_ILI9340(bool need_Display_Clear,bool need_Display_Stopped)
   if(need_Display_Clear) {
     Display_Clear_ILI9340(ILI9340_DISPLAY_CLEAR_COLOR);
   }
-  tft_ili9340.setFont(&FreeSans12pt7b);
-  tft_ili9340.setTextSize(4);
+  tft_ili9340.setFont(&FreeSans24pt7b);
+  tft_ili9340.setTextSize(2);
   if(need_Display_Stopped) {
     tft_ili9340.setTextColor(ILI9340_CYAN);
   }
@@ -783,12 +792,24 @@ void display_Timer_On_ILI9340(bool need_Display_Clear,bool need_Display_Stopped)
 }
 
 void display_Temperature_On_ILI9340(void) {
-  tft_ili9340.setTextSize(3);
+  tft_ili9340.setFont(&FreeSans18pt7b);
+  tft_ili9340.setTextSize(2);
   tft_ili9340.fillRect(ILI9340_TEMPERATURE_POS_X, ILI9340_TEMPERATURE_POS_Y-ILI9340_TEMPERATURE_HEIGTH, ILI9340_TEMPERATURE_WIDTH, ILI9340_TEMPERATURE_HEIGTH+ILI9340_TEMPERATURE_Y_OFFSET, ILI9340_DISPLAY_CLEAR_COLOR);
   tft_ili9340.setTextColor(ILI9340_RED);
   tft_ili9340.setCursor(ILI9340_TEMPERATURE_POS_X, ILI9340_TEMPERATURE_POS_Y);
   tft_ili9340.print(temperature_String_V2);
   tft_ili9340.print(F(" *C"));  
+}
+
+void display_Milli_Volt_On_ILI9340(void) {
+  tft_ili9340.setFont(&FreeSans12pt7b);
+  tft_ili9340.setTextSize(1);
+  tft_ili9340.fillRect(ILI9340_MILLI_VOLT_POS_X, ILI9340_MILLI_VOLT_POS_Y-ILI9340_MILLI_VOLT_HEIGTH, ILI9340_MILLI_VOLT_WIDTH, ILI9340_MILLI_VOLT_HEIGTH+ILI9340_MILLI_VOLT_Y_OFFSET, ILI9340_DISPLAY_CLEAR_COLOR);
+  tft_ili9340.setTextColor(ILI9340_MAGENTA);
+  tft_ili9340.setCursor(ILI9340_MILLI_VOLT_POS_X, ILI9340_MILLI_VOLT_POS_Y);
+  tft_ili9340.print(F("Voltage: "));
+  tft_ili9340.print(volt_String);
+  tft_ili9340.print(F(" mV"));
 }
 
 void Display_Clear_ILI9340(uint16_t color) {
